d6fcddeafeecebe175f575c0589c799039befd4b
ManelTP97 <97608268+ManelTP97@users.noreply.github.com>
2022-03-23 12:01:04 +0100

    Update Monitor.py

--- Monitor.py
+++ Monitor.py
@@ -6,45 +6,42 @@ import busio
 import adafruit_sgp30
 
 
-class Monitor:
+class Monitoring:
     def __init__(self):
         self.i2c = board.I2C()
-        self.i2c2 = busio.I2C(board.SCK, board.SDA, frequency=100000)
-        self.GPIO.setmode(GPIO.BCM)
+
+        GPIO.setmode(GPIO.BCM)
 
         self.pinVent = 22
-        self.GPIO.setup(self.pinvent, GPIO.OUT)
+        GPIO.setup(self.pinVent, GPIO.OUT)
 
         self.pinDoor = 27
-        self.GPIO.setup(self.pinDoor, GPIO.OUT)
+        GPIO.setup(self.pinDoor, GPIO.OUT)
 
         self.pinHall = 17
-        self.GPIO.setup(self.pinhall, GPIO.IN)
-        self.GPIO.add_event_detected(self.pinhall, GPIO.RISING)
+        GPIO.setup(self.pinHall, GPIO.IN)
+        GPIO.add_event_detect(self.pinHall, GPIO.RISING)
 
-        self.sensor = adafruit_bme680.Adafruit_BME680_I2C(self.i2c)
-
-        self.sgp30 = adafruit_sgp30.Adafruit_SGP30(self.i2c2)
         self.sps = SPS30(1)
-        self.sps.start_measurement()
 
-    #BME680 Temperatur  und Gas
+    # BME680 Temperatur  und Gas
     def getTemp(self):
+        self.sensor = adafruit_bme680.Adafruit_BME680_I2C(self.i2c)
         return self.sensor.temperature
 
     def getGas(self):
         return self.sensor.gas
 
     def getHumidity(self):
-         return self.sensor.humidity
+        return self.sensor.humidity
 
     def getPressure(self):
-         return self.sensor.pressure
+        return self.sensor.pressure
 
-
-    #SPS30 Feinstaub und Gas als Liste
+    # SPS30 Feinstaub und Gas als Liste
     def getPM(self):
         self.PM = []
+        self.sps.start_measurement()
 
         while True:
 
@@ -62,6 +59,7 @@ class Monitor:
 
     def getNC(self):
         self.NC = []
+        self.sps.start_measurement()
 
         while True:
 
@@ -78,29 +76,37 @@ class Monitor:
 
         return self.NC
 
-
-    #SGP30 eCO2 und TVOC
+    # SGP30 eCO2 und TVOC
     def geteCO2(self):
-        return self.sgp30.geteCO2
+        self.i2c2 = busio.I2C(board.SCK, board.SDA, frequency=100000)
+        self.sgp30 = adafruit_sgp30.Adafruit_SGP30(self.i2c2)
+        return self.sgp30.eCO2
 
     def getTVOC(self):
+        self.i2c2 = busio.I2C(board.SCK, board.SDA, frequency=100000)
+        self.sgp30 = adafruit_sgp30.Adafruit_SGP30(self.i2c2)
         return self.sgp30.TVOC
 
-    #Lüfter
+    # Lüfter
     def ventOn(self):
         GPIO.output(self.pinvent, 1)
 
     def ventOff(self):
         GPIO.output(self.pinvent, 0)
 
-    #Magnetschloss
+    # Magnetschloss
     def doorOpen(self):
         GPIO.output(self.pindoor, 0)
 
     def doorClose(self):
         GPIO.output(self.pindoor, 1)
 
-    #Hall-Sensor
+    # Hall-Sensor
     def getDoor(self):
+        # 0 = auf, 1 = zu
+        self.door = 0
+
         if GPIO.event_detected(self.pinhall):
-            print("Hey")
+            self.door = 1
+
+        return self.door
